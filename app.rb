102cff80c2c9ca578d7f59dc4f01167432b08dae
Stephan Yu <Stephan.yu@gmail.com>
2017-05-12 15:14:26 +0100

    Init commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'rspotify'
+require 'pry-byebug'
+require './artists.rb'
+require './secret.rb'
+
+client_id = Secret::ID
+client_token = Secret::Token
+RSpotify::authenticate(client_id, client_token)
+
+artists = Artist::ARTISTS
+artists_on_spotify = []
+artists_batch = artists.each_slice(50).to_a
+
+artists_batch.each do |sub_array|
+  sleep 5
+  sub_array.each do |artist|
+    artist_exists = RSpotify::Artist.search(artist)
+    artists_on_spotify << artist_exists unless artist_exists.empty?
+  end
+end
+
+puts artists_on_spotify.count
